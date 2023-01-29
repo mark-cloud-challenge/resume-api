245ad84ee05fb13c2d98aa2c304389fbb97b9669
Mark Guiang <mahikgot@gmail.com>
2023-01-29 23:02:06 +0800

    initial API, GET \hits

---
+++ app.py
@@ -0,0 +1,22 @@
+from connect import connect_with_connector
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+@app.before_first_request
+def init_db() -> sqlalchemy.engine.base.Engine:
+    global db
+    db = connect_with_connector()
+    return db
+
+@app.route("/hits", methods=["GET"])
+def get_hits() -> int:
+    with db.connect as conn:
+        hits = conn.execute(
+                "SELECT hits IN counter;"
+                ).fetchone()
+
+    return hits
+
+if __name__ == '__main__':
+    app.run(port=8080)

c4bd78bcf8d3817743ca1adbdd987e726ef30d57
Mark Guiang <mahikgot@gmail.com>
2023-01-30 19:01:53 +0800

    wrap query in sqlalchemy.text()

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from connect import connect_with_connector
-from flask import Flask, Response
-import sqlalchemy
+from flask import Flask
+import sqlalchemy 
 
 app = Flask(__name__)
 
@@ -13,9 +13,8 @@ def init_db() -> sqlalchemy.engine.base.Engine:
 @app.route("/hits", methods=["GET"])
 def get_hits() -> int:
     with db.connect() as conn:
-        hits = conn.execute(
-                "SELECT hits IN counter;"
-                ).fetchone()
+        query = sqlalchemy.text("SELECT hits IN counter")
+        hits = conn.execute(query).fetchone()
 
     return hits
 

d6803b4e741d44eb7231f42071bd1d8115fb311a
Mark Guiang <mahikgot@gmail.com>
2023-01-30 19:26:53 +0800

    GET /hits returns count of unique visitors

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def init_db() -> sqlalchemy.engine.base.Engine:
 @app.route("/hits", methods=["GET"])
 def get_hits() -> int:
     with db.connect() as conn:
-        query = sqlalchemy.text("SELECT hits FROM counter")
+        query = sqlalchemy.text("SELECT COUNT(visits) FROM counter")
         hits = conn.execute(query).fetchone()
 
     return hits

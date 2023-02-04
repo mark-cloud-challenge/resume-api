dc188719804acec6ecdf077baf363ba65a1ca470
unknown <mahikgot@gmail.com>
2023-02-04 23:59:46 +0800

    try new execute parameters

--- app.py
+++ app.py
@@ -37,8 +37,8 @@ def get_hits():
             return { "unique-visitors": hits[0]}
 
 def is_unique(table, column, value, conn):
-    query = sqlalchemy.text("SELECT EXISTS(SELECT 1 FROM :table WHERE :column=:value LIMIT 1)")
-    isUnique = conn.execute(query, table=table, column=column, value=value).fetchone()
+    query = sqlalchemy.text("SELECT EXISTS(SELECT 1 FROM :tableName WHERE :column=:value LIMIT 1)")
+    isUnique = conn.execute(query, tableName=table, column=column, value=value).fetchone()
 
     return isUnique[0]
 
